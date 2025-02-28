8a27f7cc46169198617f60559b04338cbe4942dd
Kevin <kevinelie1998@gmail.com>
2025-02-28 11:43:47 +0100

    Finished app/services tests

--- front/src/app/services/teacher.service.spec.ts
+++ front/src/app/services/teacher.service.spec.ts
@@ -1,16 +1,27 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { TeacherService } from './teacher.service';
+import { Teacher } from '../interfaces/teacher.interface';
+import { of } from 'rxjs';
 
 describe('TeacherService', () => {
   let service: TeacherService;
+  let httpClientMock: jest.Mocked<HttpClient>;
 
   beforeEach(() => {
+    httpClientMock = {
+      get: jest.fn()
+    } as unknown as jest.Mocked<HttpClient>;
+
     TestBed.configureTestingModule({
       imports:[
         HttpClientModule
+      ],
+      providers: [
+        TeacherService,
+        {provide: HttpClient, useValue: httpClientMock}
       ]
     });
     service = TestBed.inject(TeacherService);
@@ -18,5 +29,22 @@ describe('TeacherService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should retrieve the teacher informations', (done) => {
+    const mockTeacher: Teacher = {
+      id: 1,
+      firstName: 'Margaux',
+      lastName: 'Dupont',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+    httpClientMock.get.mockReturnValue(of(mockTeacher));
+
+    service.detail('1').subscribe((teacher) => {
+      expect(teacher).toEqual(mockTeacher);
+      expect(httpClientMock.get).toBeCalledWith('api/teacher/1');
+      done();
+    });
   });
 });
--- front/src/app/services/user.service.spec.ts
+++ front/src/app/services/user.service.spec.ts
@@ -1,16 +1,27 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { UserService } from './user.service';
+import { of } from 'rxjs';
+import { User } from '../interfaces/user.interface';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpClientMock: jest.Mocked<HttpClient>;
 
   beforeEach(() => {
+    httpClientMock = {
+      delete: jest.fn()
+    } as unknown as jest.Mocked<HttpClient>;
+
     TestBed.configureTestingModule({
       imports:[
         HttpClientModule
+      ],
+      providers: [
+        UserService,
+        {provide: HttpClient, useValue: httpClientMock}
       ]
     });
     service = TestBed.inject(UserService);
@@ -19,4 +30,24 @@ describe('UserService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should delete the user', (done) => {
+    const mockUser: User = {
+      id: 1,
+      email: 'test@user.com',
+      firstName: 'Margaux',
+      lastName: 'Dupont',
+      admin: true,
+      password:'test!1234',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+    httpClientMock.delete.mockReturnValue(of(mockUser));
+    
+    service.delete('1').subscribe((user) => {
+      expect(user).toEqual(mockUser);
+      expect(httpClientMock.delete).toBeCalledWith('api/user/1');
+      done();
+    });
+  })
 });
