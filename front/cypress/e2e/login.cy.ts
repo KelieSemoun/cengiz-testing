b33ae05ef4dd774a35ad3aad395ce492f70ceb79
Kevin <kevinelie1998@gmail.com>
2025-03-06 11:27:28 +0100

    Added E2E incomplete login form test

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,16 +1,26 @@
 describe('Login spec', () => {
-  it('Login successfull', () => {
+  beforeEach(() => {
     cy.visit('/login')
 
-    cy.intercept('POST', '/api/auth/login', {
-      body: {
-        id: 1,
-        username: 'userName',
-        firstName: 'firstName',
-        lastName: 'lastName',
-        admin: true
-      },
-    })
+    cy.intercept('POST', '/api/auth/login', (req) => {
+      if (req.body.email === "yoga@studio.com" && req.body.password === "test!1234") {
+        req.reply({
+          statusCode: 200,
+          body: {
+            id: 1,
+            username: 'userName',
+            firstName: 'firstName',
+            lastName: 'lastName',
+            admin: true
+          }
+        });
+      } else {
+        req.reply({
+          statusCode: 401,
+          body: { message: "Invalid credentials" }
+        });
+      }
+    });
 
     cy.intercept(
       {
@@ -18,10 +28,17 @@ describe('Login spec', () => {
         url: '/api/session',
       },
       []).as('session')
-
+  })
+  it('Login successfull', () => {
     cy.get('input[formControlName=email]').type("yoga@studio.com")
     cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
 
     cy.url().should('include', '/sessions')
+  })
+
+  it('Login fails upon incomplete forms', () => {
+    cy.get('input[formControlName=email]').type(`${"yoga@studio.com"}{enter}{enter}`)
+
+    cy.get('p.error').should('be.visible').and('contain.text', 'An error occurred');
   })
 });
